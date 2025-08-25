5441ecc9f99fc1121d954f9d725bb8a49f2d6e08
mehakkansal06 <mehak8564.beaift25@chitkara.edu.in>
2025-08-26 00:48:25 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,43 @@
+#loops in python 
+for i in range (1, 1000):
+    print("numbers:", i) 
+    
+    
+fruits=["cherry","mango","guavavvvavav"]
+print(fruits[0]) 
+print(fruits)   
+for fruit in fruits:
+    print("I Like:", fruit)
+    
+    #while loop:
+    #code block
+    
+    i=1
+    while i<=5:
+        print("number :",i)
+        i+=1
+
+#practice:
+
+   #1) NATURAL NUMBERS:
+for i in range (1,11):
+    print("Natural number",i)
+    
+i=1
+while i<=10 :
+    print("natural number:", i)
+    i+=1
+    
+#2)MULTIPLICATION TABLE OF 5:
+i=5
+while i<=50 :
+    print (i)
+    i+=5
+
+
+#even numbers :
+i=2
+while i<=100 :
+    print(i)
+    i+=2
+     
